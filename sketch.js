262c543e155c702b6bf9562fedcb2bbb9bb84f59
Morgan Green <cellardoor.morgan@gmail.com>
2025-01-18 15:48:17 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,8 @@
 function setup() {
+  // remove p5 canvas
+  canvas = document.querySelector('canvas');
+	canvas.remove();
+  // create svg canvas
   cvs = SVG().addTo('main').size(windowWidth, windowHeight);
 }
 
